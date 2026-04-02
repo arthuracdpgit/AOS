c75d84e04b5f4c78152a2dbea2d6d6e7d45b1221
Arthuracdpgit <arthur.unicap@gmail.com>
2026-04-01 21:15:46 -0300

    Completando CRUD de User e Message

--- express-01/api/routes/message.js
+++ express-01/api/routes/message.js
@@ -1,35 +1,30 @@
-import { Router } from "express";
-import { v4 as uuidv4 } from "uuid";
+import { Router } from 'express';
 
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   const messages = await req.context.models.Message.findAll();
   return res.send(messages);
 });
 
-router.get("/:messageId", async (req, res) => {
-  const message = await req.context.models.Message.findByPk(
-    req.params.messageId,
-  );
+router.get('/:messageId', async (req, res) => {
+  const message = await req.context.models.Message.findByPk(req.params.messageId);
   return res.send(message);
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const message = await req.context.models.Message.create({
     text: req.body.text,
     userId: req.context.me.id,
   });
-
   return res.send(message);
 });
 
-router.delete("/:messageId", async (req, res) => {
+router.delete('/:messageId', async (req, res) => {
   const result = await req.context.models.Message.destroy({
     where: { id: req.params.messageId },
   });
-
-  return res.send(true);
+  return res.send(result !== 0);
 });
 
 export default router;
