7ee520c326c0e2012c262b28f36e5bac13f56a39
Arthuracdpgit <arthur.unicap@gmail.com>
2026-04-19 20:29:11 -0300

    Ajuste de SSL para Vercel

--- express-01/api/models/index.js
+++ express-01/api/models/index.js
@@ -7,7 +7,12 @@ import getTarefaModel from "./tarefa";
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
-  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 const models = {
